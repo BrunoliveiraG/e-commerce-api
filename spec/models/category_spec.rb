cd7b0fd99d8723d52b45f0a1ca0945a0e425a819
brunogomesbx <brunogomes@bxblue.com.br>
2021-12-16 13:06:59 -0300

    improving user model to have tests and field validations

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,5 +6,4 @@ RSpec.describe Category, type: :model do
 
   it { is_expected.to have_many(:product_categories).dependent(:destroy) }
   it { is_expected.to have_many(:products).through(:product_categories) }
-
 end
