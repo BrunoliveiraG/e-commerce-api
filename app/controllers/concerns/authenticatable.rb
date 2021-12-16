82e6e4ce1b372d5d1a634135c29d6d56bb03f47d
brunogomesbx <brunogomes@bxblue.com.br>
2021-12-16 09:17:35 -0300

    configuring authentication

--- app/controllers/concerns/authenticatable.rb
+++ app/controllers/concerns/authenticatable.rb
@@ -1,9 +1,8 @@
-module Authenticable
+module Authenticatable
   extend ActiveSupport::Concern
 
   included do
     include DeviseTokenAuth::Concerns::SetUserByToken
-
     before_action :authenticate_user!
   end
 end
