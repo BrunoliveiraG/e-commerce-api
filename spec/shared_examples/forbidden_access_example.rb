02d1a44660cd447d6f8167b4017bff2084b9e42c
brunogomesbx <brunogomes@bxblue.com.br>
2021-12-29 16:59:58 -0300

    add search by name to category

--- spec/shared_examples/forbidden_access_example.rb
+++ spec/shared_examples/forbidden_access_example.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-shared_examples "forbidden access" do
-  it "returns error message" do
-    expect(body_json['errors']['fields']).to eq 'Forbidden access'
+shared_examples 'forbidden access' do
+  it 'returns error message' do
+    expect(body_json['errors']['message']).to eq 'Forbidden access'
   end
 
-  it "returns forbidden status" do
+  it 'returns forbidden status' do
     expect(response).to have_http_status(:forbidden)
   end
 end
