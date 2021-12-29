c73b3a73ff242b59b6b84a760a1308f1d76547c3
brunogomesbx <brunogomes@bxblue.com.br>
2021-12-29 16:27:31 -0300

    config restriction access

--- spec/shared_examples/forbidden_access_example.rb
+++ spec/shared_examples/forbidden_access_example.rb
@@ -2,7 +2,7 @@
 
 shared_examples "forbidden access" do
   it "returns error message" do
-    expect(body_json['errors']['message']).to eq "Forbidden access"
+    expect(body_json['errors']['fields']).to eq 'Forbidden access'
   end
 
   it "returns forbidden status" do
