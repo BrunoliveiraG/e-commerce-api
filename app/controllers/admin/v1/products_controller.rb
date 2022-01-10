61e9c10e60c5ca459f843ed31b2e4fb2837d5098
brunogomesbx <brunogomes@bxblue.com.br>
2022-01-10 20:28:08 -0300

    listing api products

---
+++ app/controllers/admin/v1/products_controller.rb
@@ -0,0 +1,12 @@
+module Admin::V1
+  class ProductsController < ApiController
+    def index
+      @products = load_products
+    end
+
+    def load_products
+      permitted = params.permit({ search: :name }, { order: {} }, :page, :length)
+      Admin::ModelLoadingService.new(Product.all, permitted).call
+    end
+  end
+end
