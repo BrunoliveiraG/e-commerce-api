230ca52ad503373971a27a6242960787a38a529c
brunogomesbx <brunogomes@bxblue.com.br>
2022-01-21 23:03:36 -0300

    juno billing and auth

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,11 +1,28 @@
-RSpec.configure do |config|
+require 'active_record'
 
+module RefineRollback
+  refine ::ActiveRecord::ConnectionAdapters::TransactionManager do
+    def rollback_transaction(transaction = nil)
+      @connection.lock.synchronize do
+        transaction ||= @stack.pop
+        transaction.try(:rollback) && transaction.rollback_records
+      end
+    end
+  end
+end
+
+RSpec.configure do |config|
+  using RefineRollback
   config.before(:suite) do
-    DatabaseCleaner.clean_with :truncation, except: %w(ar_internal_metadata)
+    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
   end
 
   config.before(:each) do
