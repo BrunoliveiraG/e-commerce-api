651e6ddbcfcbd6f0658178e1ab6702ab4132f26a
brunogomesbx <brunogomes@bxblue.com.br>
2022-01-23 19:41:53 -0300

    top 5 service

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
+  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  Rails.application.routes.default_url_options[:host] = "http://localhost:3000"
 
   config.action_mailer.delivery_method = :smtp
 
