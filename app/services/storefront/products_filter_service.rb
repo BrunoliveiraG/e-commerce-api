ac1c3b5602a5a80e83a582527b659e17f1a4d8d8
brunogomesbx <brunogomes@bxblue.com.br>
2022-01-25 21:53:29 -0300

    fixing search

--- app/services/storefront/products_filter_service.rb
+++ app/services/storefront/products_filter_service.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module Storefront
   class ProductsFilterService
     attr_reader :records, :pagination
@@ -13,9 +11,9 @@ module Storefront
     def call
       set_pagination_values
       get_available_products
-      searched = filter_records.distinct('products.*')
+      searched = filter_records.select("products.*, games.mode, games.developer, games.release_date").distinct
       @records = searched.order(@params[:order].to_h).paginate(@params[:page], @params[:length])
-      set_pagination_attributes(searched.count)
+      set_pagination_attributes(searched.size)
     end
 
     private
@@ -42,16 +40,14 @@ module Storefront
     end
 
     def filter_by_search
-      return @records.all unless @params.key?(:search)
-
+      return @records.all unless @params.has_key?(:search)
       filtered_records = @records.like(:name, @params[:search])
       filtered_records = filtered_records.or(@records.like(:description, @params[:search]))
       filtered_records.or @records.merge(Game.like(:developer, @params[:search]))
     end
 
     def filter_by_categories
-      return @records.all unless @params.key?(:category_ids)
-
+      return @records.all unless @params.has_key?(:category_ids)
       @records.where(categories: { id: @params[:category_ids] })
     end
 
@@ -59,29 +55,19 @@ module Storefront
       min_price = @params.dig(:price, :min)
       max_price = @params.dig(:price, :max)
       return @records.all if min_price.blank? && max_price.blank?
-
       @records.where(price: min_price..max_price)
     end
 
     def filter_by_release_date
-      min_date = begin
-        Time.parse(@params.dig(:release_date, :min)).beginning_of_day
-      rescue StandardError
-        nil
-      end
-      max_date = begin
-        Time.parse(@params.dig(:release_date, :max)).end_of_day
-      rescue StandardError
-        nil
-      end
+      min_date = Time.parse(@params.dig(:release_date, :min)).beginning_of_day rescue nil
+      max_date = Time.parse(@params.dig(:release_date, :max)).end_of_day rescue nil
       return @records.all if min_date.blank? && max_date.blank?
-
       Game.where(release_date: min_date..max_date)
     end
 
     def set_pagination_attributes(total_filtered)
       total_pages = (total_filtered / @params[:length].to_f).ceil
-      @pagination.merge!(page: @params[:page], length: @records.count,
+      @pagination.merge!(page: @params[:page], length: @records.size,
                          total: total_filtered, total_pages: total_pages)
     end
   end
